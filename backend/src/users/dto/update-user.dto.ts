fe5dbaa53f9be2bfb89b9b6fb901dd2d3afce41b
Aparialtech <info@aparial.com>
2026-02-10 15:02:44 +0300

    fix(profile): allow avatar updates

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,23 +1,87 @@
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
 export class EmergencyContactDto {
+  @IsOptional()
+  @IsString()
   fullName?: string;
+
+  @IsOptional()
+  @IsString()
   phone?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
   address?: string;
+
+  @IsOptional()
+  @IsString()
   relation?: string;
 }
 
 export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsString()
   surname?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
   phone?: string;
+
+  @IsOptional()
+  @IsString()
   address?: string;
+
+  @IsOptional()
+  @IsString()
   gender?: string;
+
+  @IsOptional()
+  @IsString()
   birthDate?: string;
+
+  @IsOptional()
+  @IsString()
   nationalId?: string;
+
+  @IsOptional()
+  @IsBoolean()
   pushReminderEnabled?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
   emailReminderEnabled?: boolean;
+
+  @IsOptional()
+  @IsString()
   identityDocumentUrl?: string;
+
+  @IsOptional()
+  @IsString()
   avatarUrl?: string;
+
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => EmergencyContactDto)
   emergencyContact?: EmergencyContactDto;
 }
